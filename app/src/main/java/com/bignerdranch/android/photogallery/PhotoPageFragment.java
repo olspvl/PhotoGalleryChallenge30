7675cc8a82ce9ff7fe84efd66622001e7fba96af
olspvl <olspvl@gmail.com>
2018-12-12 21:50:43 +0200

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -0,0 +1,97 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.Intent;
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+
+    private Uri mUri;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mProgressBar = v.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100);
+
+        mWebView = v.findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if(newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(Uri.parse(url).getScheme().contains("http")) {
+                    return false;
+                }
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl(mUri.toString());
+        return v;
+    }
+
+    public boolean canGoBack() {
+        return mWebView != null && mWebView.canGoBack();
+    }
+
+    public void webViewGoBack() {
+        mWebView.goBack();
+    }
+
+}
